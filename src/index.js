0cd3035418444afd26e693d0cfc7f1266606d848
“Catherine <cathanders10@gmail.com>
2022-11-11 11:25:46 -0500

    made card front and back html eles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Card from "./scripts/card";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const cardEle = document.getElementById("card");
-  //grabbing ele with id of main then passing it into example class
-  new Card(cardEle);
-});
+const cardEle = document.getElementById("card");
+new Card(cardEle);
+
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,17 +1,19 @@
 // all JS code goes in an indiviual file in this script folder
 
-let cardHolder = document.getElementById("card-holder");
-
 class Card {
   constructor(ele) {
     this.ele = ele;
-    this.ele.innerHTML = "<h1>This will be a card</h1>";
 
-    this.ele.addEventListener("click", this.handleClick.bind(this));
-  }
+    const front = document.createElement("div");
+    front.setAttribute("id", "front-of-card");
 
-  handleClick() {
-    this.ele.children[0].innerText = "You flipped me!";
+    const back = document.createElement("div");
+    back.setAttribute("id", "back-of-card");
+
+    ele.appendChild(front);
+    ele.appendChild(back);
+
+
   }
 }
 
