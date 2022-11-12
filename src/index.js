89293ec84a5941ba7e0c54d2e9bb4a84ca5df274
“Catherine <cathanders10@gmail.com>
2022-11-12 17:29:27 -0500

    figuring out flipping action

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import * as allDataFetcher from "./scripts/data_fetcher";
 // import * as allCardFiller from "./scripts/card_filler";
 
 allDataFetcher.dataFetcher();
+
+const cardHolder = document.getElementById("card-holder")
+
+cardHolder.addEventListener('click', e => {
+    if (e.target.classList.contains('card')) {
+        target.toggle("flipCard");
+    }
+})
